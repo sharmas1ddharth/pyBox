26b3aeb13675179cb77f8d09eb5c4cbc42293faf
Siddharth Sharma <ssiddharth408@gmail.com>
2022-06-30 23:54:54 +0530

    solve filename argument issue

--- pybox/conversion.py
+++ pybox/conversion.py
@@ -5,20 +5,20 @@ import os
 
 def json_to_yaml(file, name=None):
     # load json data
-    with open('a.json') as f:
+    with open(file) as f:
         data = f.read()
         json_data = json.loads(data)
         # convert unicode to string
         converted_json_data = json.dumps(json_data)
 
     # output yaml
-    if name == None:
+    if name is None:
         name_, _ = os.path.splitext(file)
         yaml_data = open(f"{name_}.yaml", 'w')
         yaml_data.write(yaml.dump(yaml.safe_load(converted_json_data), default_flow_style=False))
         yaml_data.close()
 
-    elif name != None:
+    elif name is not None:
         name_, _ = os.path.splitext(name)
         yaml_data = open(f"{name_}.yaml", 'w')
         yaml_data.write(yaml.dump(yaml.safe_load(converted_json_data), default_flow_style=False))
