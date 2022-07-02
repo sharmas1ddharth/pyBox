4d76a454c767c470d37cfbf2c3966aeb41a48f15
Siddharth Sharma <ssiddharth408@gmail.com>
2022-07-02 19:55:19 +0530

    added ability to work with cwd when not providing directory path

--- pybox/files.py
+++ pybox/files.py
@@ -1,8 +1,11 @@
 import os
 
 
-def rename_with(dir_path=None, name="pybox"):
+def rename_with(dir_path="", name="pybox"):
     """rename all files with temp in front of file name"""
+    if dir_path == "":
+        dir_path = os.getcwd()
+
     for count, f in enumerate(os.listdir(dir_path)):
         f_name, f_ext = os.path.splitext(f)
         f_name = name + str(count)
@@ -10,8 +13,11 @@ def rename_with(dir_path=None, name="pybox"):
         os.rename(f"{dir_path}/{f}", new_name)
 
 
-def rename_to_numbers(dir_path, show=False):
+def rename_to_numbers(dir_path="", show=False):
     """rename all files with numbers"""
+    if dir_path == "":
+        dir_path = os.getcwd()
+
     for count, f in enumerate(os.listdir(dir_path)):
         f_name, f_ext = os.path.splitext(f)
         f_name = str(count)
