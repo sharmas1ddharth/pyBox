1d0d44193ade8c086664c12e12db1d6b5994dc37
Siddharth Sharma <ssiddharth408@gmail.com>
2022-07-02 22:38:08 +0530

    search function added to files

--- pybox/files.py
+++ pybox/files.py
@@ -26,3 +26,42 @@ def rename_to_numbers(dir_path="", show=False):
         if show:
             print(f'Old: {f}, New: {new_name}')
 
+
+import os
+
+
+def remove_(path, type_):
+    if type_ == "file":
+        os.remove(path)
+    elif type_ == "dir":
+        os.rmdir(path)
+
+
+def search(name="", dir_name="", type_="file", remove=False, print_=True):
+    if dir_name == "":
+        dir_name = os.getcwd()
+
+    dir_path = os.path.dirname(os.path.realpath(dir_name))
+
+    for root, dirs, files in os.walk(dir_path):
+        type_to_search = None
+
+        if type_ == "file":
+            type_to_search = files
+        elif type_ == "dir":
+            type_to_search = dirs
+
+        for item in type_to_search:
+            try:
+                if item.endswith(name):
+                    path = root + '/' + str(item)
+                    print(path)
+                    if remove:
+                        remove_(path, type_)
+
+            except:
+                if item == name:
+                    path = root + '/' + str(item)
+                    print(path)
+                    if remove:
+                        remove_(path, type_)
